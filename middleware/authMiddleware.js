cb4116db04f9eeeb5de52f2d84628c0cf965c7f8
Mads Brandt <mads.brandt93@gmail.com>
2020-09-04 22:54:44 +0200

    Removed optional chaining

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,7 @@ module.exports = (context) => {
   if (context.connection) {
     context = context.connection.context;
   } else {
-    if (context.req?.headers.authorization) {
+    if (context.req.headers.authorization) {
       const token = context.req.headers.authorization.split('Bearer ')[1];
       jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
         if (err) throw new AuthenticationError('Invalid Token');
